d2c80b51fc5e1ba5dbe2ca1218829b649b430367
Jed <jed@m6700.local>
2018-09-02 00:13:59 -0400

    jed@m6700: Sun Sep  2 04:13:59 UTC 2018

--- C/4.c
+++ C/4.c
@@ -3,14 +3,10 @@
 #include <stdio.h>
 #include <string.h>
 
-void car_init2(struct Car *car, char *plate) { strcpy(car->license_plate, "Unknown"); }
+void car_init2(struct Car *car, char *plate) { strcpy(car->license_plate, plate); }
 
 int main() {
   struct Car c1;
-  car_init(&c1);
-  printf("Plate Number: %s\n", c1.license_plate);
-
-
   car_init2(&c1, "foo");
   printf("Plate Number: %s\n", c1.license_plate);
   return 0;
