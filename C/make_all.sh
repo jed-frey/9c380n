63f9908f4b0e3d043421acee476c3bbea1b536b8
Jed <jed@m6700.local>
2018-09-01 22:39:45 -0400

    jed@m6700: Sun Sep  2 02:39:45 UTC 2018

---
+++ C/make_all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env tcsh
+
+foreach CLANG_VER ( 3.8 3.9 4.0 5.0 6.0 7 )
+
+echo $CLANG_VER
+#command1
+#command2
+end
+
+foreach GCC_VER ( 4.8 5 6 7 8 )
+
+echo $GCC_VER
+#command1
+#command2
+end
+
