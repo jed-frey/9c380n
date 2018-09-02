face63457b8c3dfba27bd78c64674360beb7c757
Jed <jed@m6700.local>
2018-09-01 23:31:15 -0400

    jed@m6700: Sun Sep  2 03:31:15 UTC 2018

--- C/3.c
+++ C/3.c
@@ -2,10 +2,5 @@
 #include <stdio.h>
 
 int main() {
-  struct Point p1;
-  unsigned int idx = 0;
-  for (idx = 1; idx <= 10; idx += 1) {
-    printf("I'm on loop %d\n", idx);
-  }
-  return 0;
+  struct Car c1;
 }
--- C/3.h
+++ C/3.h
@@ -6,4 +6,4 @@ struct Car {
   char license_plate[10];
 };
 
-Car car_init();
+struct Car car_init();
