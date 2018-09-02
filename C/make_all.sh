d89be949a17b50763227200058c0594d17b1f07e
Jed <jed@m6700.local>
2018-09-01 23:05:03 -0400

    jed@m6700: Sun Sep  2 03:05:03 UTC 2018

--- C/make_all.sh
+++ C/make_all.sh
@@ -2,12 +2,15 @@
 
 foreach CLANG_VER ( 3.8 3.9 4.0 5.0 6.0 7 )
 setenv CC clang-${CLANG_VER}
+
 setenv BIN 1.clang-${CLANG_VER}.bin
 make ${BIN}
 
-setenv BIN 1.clang-${CLANG_VER}.bin
 setenv PPO 1.clang-${CLANG_VER}.pp
 make ${PPO}
+
+setenv LL 1.clang-${CLANG_VER}.ll
+make ${LL}
 end
 
 foreach GCC_VER ( 4.8 5 6 7 8 )
