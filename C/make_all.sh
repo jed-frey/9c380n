63bbc20d6aa3846c4914591b795ada964f4728cd
Jed <jed@m6700.local>
2018-09-01 23:15:15 -0400

    jed@m6700: Sun Sep  2 03:15:15 UTC 2018

--- C/make_all.sh
+++ C/make_all.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env tcsh
 
 foreach C_SRC (1 2)
+setenv SRC ${C_SRC}.c
 
 foreach CLANG_VER ( 3.8 3.9 4.0 5.0 6.0 7 )
 setenv CC clang-${CLANG_VER}
 
-setenv BIN 1.clang-${CLANG_VER}.bin
+setenv BIN ${C_SRC}.clang-${CLANG_VER}.bin
 make ${BIN}
 
-setenv PPO 1.clang-${CLANG_VER}.pp
+setenv PPO ${C_SRC}.clang-${CLANG_VER}.pp
 make ${PPO}
 
-setenv LL 1.clang-${CLANG_VER}.ll
+setenv LL ${C_SRC}.clang-${CLANG_VER}.ll
 make ${LL}
 end
 
 foreach GCC_VER ( 4.8 5 6 7 8 )
-setenv BIN 1.gcc-${GCC_VER}.bin
+setenv BIN ${C_SRC}.gcc-${GCC_VER}.bin
 setenv CC gcc-${GCC_VER}
+
 make ${BIN}
 end
 
+end
