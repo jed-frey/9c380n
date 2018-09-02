a73fea12499a600a51563e7c3af16fe472c1ec1a
Jed <jed@m6700.local>
2018-09-01 23:24:41 -0400

    jed@m6700: Sun Sep  2 03:24:41 UTC 2018

--- C/3.c
+++ C/3.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "3.h"
+
 int main() {
+	struct Point p1;
   unsigned int idx = 0;
   for (idx = 1; idx <= 10; idx += 1) {
     printf("I'm on loop %d\n", idx);
--- C/3.h
+++ C/3.h
@@ -2,3 +2,8 @@ struct Point
 {
    int x, y;
 };
+
+struct Point
+{
+   int x, y;
+};
